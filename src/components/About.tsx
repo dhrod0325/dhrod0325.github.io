495689e951335c1b6af983c23f4c3602c313d6eb
설아아빠 <dhrod0325@naver.com>
2022-03-31 19:12:24 +0900

    update

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,10 +4,7 @@ export const About: FC = () => {
   return (
     <div className="p-4 mb-3 bg-light rounded">
       <h4 className="fst-italic">About</h4>
-      <p className="mb-0">
-        Customize this section to tell your visitors a little bit about your publication, writers,
-        content, or something else entirely. Totally up to you.
-      </p>
+      <p className="mb-0">today better than yesterday</p>
     </div>
   );
 };
