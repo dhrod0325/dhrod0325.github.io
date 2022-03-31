0fcec7dd50b48640cfda26f650670e1206c01143
설아아빠 <dhrod0325@naver.com>
2022-03-31 19:10:16 +0900

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { Contact } from '@/components/Contact';
 import { Archives } from '@/components/Archives';
 import { About } from '@/components/About';
 import { Posts } from '@/components/Posts';
+import { ThumbCard } from '@/components/ThumbCard';
 
 export const Index: FC = () => {
   const posts = new PostsWrapper();
@@ -18,23 +19,8 @@ export const Index: FC = () => {
       </div>
 
       <div className="row mb-2">
-        {firstPosts.map(({ title, category, date, summary, thumb }) => (
-          <div className="col-md-6" key={date.toLocaleString()}>
-            <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-              <div className="col p-4 d-flex flex-column position-static">
-                <strong className="d-inline-block mb-2 text-primary">{category}</strong>
-                <h3 className="mb-0">{title}</h3>
-                <div className="mb-1 text-muted">{date.toLocaleString()}</div>
-                <p className="card-text mb-auto">{summary}</p>
-                <a href="#" className="stretched-link">
-                  Continue reading
-                </a>
-              </div>
-              <div className="col-auto d-none d-lg-block">
-                <img src={thumb} alt="" style={{ height: `100%` }} />
-              </div>
-            </div>
-          </div>
+        {firstPosts.map(post => (
+          <ThumbCard post={post} key={post.date.toLocaleString()} />
         ))}
       </div>
 
@@ -47,9 +33,7 @@ export const Index: FC = () => {
         <div className="col-md-4">
           <div className="position-sticky" style={{ top: '2rem' }}>
             <About />
-
             <Archives />
-
             <Contact />
           </div>
         </div>
